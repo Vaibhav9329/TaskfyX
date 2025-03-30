a3d671108e174910fbae6dcee02a4f1f1037e9b3
vaibhav <khotvaibhav900@gamil.com>
2025-03-30 22:12:16 +0530

    initial and seting up project and normalize line endings

---
+++ src/main/java/com/taskfyx/app/TaskifyXApplication.java
@@ -0,0 +1,13 @@
+package com.taskfyx.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskifyXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskifyXApplication.class, args);
+	}
+
+}
